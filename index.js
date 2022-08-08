28bbb641bf2f6c58e198462ce69cc6eeeff85937
Sabrina Kramer <01CodePathways11@gmail.com>
2022-08-08 16:15:20 -0500

    Create constructor, connect button, add dummy data

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 let books = ["Book One", "Book Two", "Book Three"];
 
-function Book(){
-    //constructor
+function Book(title, author, numPages){
+    title = title;
+    author = author;
+    numPages = numPages;
 }
 
 function addBookToLibrary(bookArr){
     bookArr.foreach(book => console.log(book));
 }
+
+function openMenu(){
+    console.log("This event listener is connected")
+}
+
+let bookList = document.getElementsByClassName("book-list");
+let addBtn = document.getElementById("add-btn")
+
+addBtn.addEventListener("click", openMenu);
+
+
