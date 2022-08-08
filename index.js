539af8b6bf4d9892324b546d43a3d21cb2dcb7cb
Sabrina Kramer <01CodePathways11@gmail.com>
2022-08-08 15:58:41 -0500

    Add content & styling

---
+++ index.js
@@ -0,0 +1,9 @@
+let books = ["Book One", "Book Two", "Book Three"];
+
+function Book(){
+    //constructor
+}
+
+function addBookToLibrary(bookArr){
+    bookArr.foreach(book => console.log(book));
+}
